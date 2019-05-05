2a38d6e7020409b423e9e7cff1c4a4aaea615900
Frago9876543210 <al.volf.av@gmail.com>
2019-05-05 17:51:28 +0300

    oops

---
+++ src/Frago9876543210/PocketBash/BashCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Frago9876543210\PocketBash;
+
+use pocketmine\{command\Command, command\CommandSender, command\utils\InvalidCommandSyntaxException};
+use function count;
+use function implode;
+
+class BashCommand extends Command{
+	/** @var Main */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		parent::__construct("bash", "Allows to execute bash code in your server", "/bash <commands>");
+		$this->plugin = $plugin;
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+		if(count($args) === 0){
+			throw new InvalidCommandSyntaxException();
+		}
+
+		$this->plugin->setSender($sender);
+		$this->plugin->getThread()->write(implode(" ", $args));
+	}
+}
